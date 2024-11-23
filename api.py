1e761cff7a117dba4abcff35e5de37a4f5596bfb
Isha Motiyani <130890395+motiyaniisha22@users.noreply.github.com>
2024-11-23 18:31:11 +0530

    Update api.py

--- api.py
+++ api.py
@@ -19,10 +19,14 @@ prompt = PromptTemplate(
     input_variables=['context', 'question']
 )
 
+print("#### Prompt Template ready !!")
+
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2",
     model_kwargs={'device': 'cuda'}
 )
+
+print("#### Embeddings model loaded !!")
 
 db = FAISS.load_local(
     DB_FAISS_PATH,
@@ -30,11 +34,17 @@ db = FAISS.load_local(
     allow_dangerous_deserialization=True
 )
 
+print("#### FAISS Database loaded !!")
+
 model = AutoModelForCausalLM.from_pretrained("neuralmagic/Meta-Llama-3.1-8B-Instruct-FP8").to('cuda')
-tokenizer = AutoTokenizer.from_pretrained("neuralmagic/Meta-Llama-3.1-8B-Instruct-FP8", max_new_tokens = 1024)
+tokenizer = AutoTokenizer.from_pretrained("neuralmagic/Meta-Llama-3.1-8B-Instruct-FP8")
+
+print("#### Model & Tokenizer loaded !!")
 
 pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, device=0, max_new_tokens = 1024)
 llm = HuggingFacePipeline(pipeline=pipe)
+
+print("#### Pipeline created !!")
 
 # Create the sum chain once
 sum_chain = RetrievalQA.from_chain_type(
@@ -44,6 +54,8 @@ sum_chain = RetrievalQA.from_chain_type(
     return_source_documents=False,
     chain_type_kwargs={'prompt': prompt}
 )
+
+print("#### Summary generated successfully !!")
 
 def generate_response(query):
     response = sum_chain({'query': query})
