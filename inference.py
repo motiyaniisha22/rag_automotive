be23cec92534a4834e9161860933a3047eb44bfb
Isha Motiyani <130890395+motiyaniisha22@users.noreply.github.com>
2024-11-23 18:03:54 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,9 +1,10 @@
 import api
 
-while True:
-    user_input = input("Paste Json here: ")
-    if user_input == "exit":
-        break
-    response = api.generate_response(user_input)
-    print(response)
+# while True:
+#     user_input = input("Paste Json here: ")
+#     if user_input == "exit":
+#         break
+user_input = """{ ‘make’: ‘ford’, ‘model’: ‘escape, ‘year’: ‘2001’, ‘issue’: ‘stuck throttle risk’}"""
+response = api.generate_response(user_input)
+print(response)
 
