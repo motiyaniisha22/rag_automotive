bf2dee5177138aae7510a29940f6d2678b6954b4
Isha Motiyani <130890395+motiyaniisha22@users.noreply.github.com>
2024-11-24 15:11:54 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,14 +1,16 @@
 import time
+import api
 
-# user_input = "{'make': 'ford', 'model' : 'escape', 'year': '2001', 'issue': 'stuck throttle risk'}"
+user_input = "{'make': 'ford', 'model' : 'escape', 'year': '2001', 'issue': 'stuck throttle risk'}"
 
-while True:
-    user_input = input("Paste Json here: ")
-    start_time = time.time()
-    response, retrieved_docs = generate_response(user_input)
-    end_time = time.time()
-    print("Summary :\n",response)
-    print("\n\nRetrieved Documents :\n",retrieved_docs)
-    print("\n\nTime taken = ", (end_time - start_time) // 60, "minutes")
-    if user_input == "exit":
-        break
+# while True:
+#     user_input = input("Paste Json here: ")
+#     if user_input == "exit":
+#         break
+
+# start_time = time.time()
+response, retrieved_docs = api.generate_response(user_input)
+# end_time = time.time()
+print("Summary :\n",response)
+print("\n\nRetrieved Documents :\n",retrieved_docs)
+# print("\n\nTime taken = ", (end_time - start_time) // 60, "minutes")
