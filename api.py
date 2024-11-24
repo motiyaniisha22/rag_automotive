2d17b5f1a80f85b8aa1b526651bce0a7473c89f2
Isha Motiyani <130890395+motiyaniisha22@users.noreply.github.com>
2024-11-24 15:04:32 +0530

    Update api.py

--- api.py
+++ api.py
@@ -11,24 +11,11 @@ warnings.filterwarnings('ignore')
 
 DB_FAISS_PATH = 'data/db_faiss'
 
-# custom_prompt_template = """You are a summarization agent that summarizes an automotive issue.
-# Inputs will be as follows -
-#     1) Question as a JSON object with keys : make (company of the car), model (model of the car), year (year of the model launch) and issue (issue with the model).
-#     2) Context (based on which the summary will be genrated)
-
-# Context: {context}
-# Question: {question}
-
-# Output should strictly follow the following format :
-# ```
-# Summary:
-# ```
-# """
-
-custom_prompt_template = """You are an expert in creating summaries for automotive issues based on some context documents. You will be given a json as input, generate only a very short summary.
-Question : {question}
+custom_prompt_template = """You are an expert in generating short summary for automotive issues.
+Based on the issue given in input, you will be provided with a context text that needs to summarized and given as output.
+Generate a very short abstractive summary of the context. 
 Context : {context}
-
+Question : {question}
 Summary:"""
 
 prompt = PromptTemplate(
@@ -36,14 +23,14 @@ prompt = PromptTemplate(
     input_variables=['context', 'question']
 )
 
-print("#### Prompt Template ready !!")
+# print("#### Prompt Template ready !!")
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2",
     model_kwargs={'device': 'cuda'}
 )
 
-print("#### Embeddings model loaded !!")
+# print("#### Embeddings model loaded !!")
 
 db = FAISS.load_local(
     DB_FAISS_PATH,
@@ -51,36 +38,43 @@ db = FAISS.load_local(
     allow_dangerous_deserialization=True
 )
 
-print("#### FAISS Database loaded !!")
+# print("#### FAISS Database loaded !!")
 
 model = AutoModelForCausalLM.from_pretrained("neuralmagic/Meta-Llama-3.1-8B-FP8").to('cuda')
 tokenizer = AutoTokenizer.from_pretrained("neuralmagic/Meta-Llama-3.1-8B-FP8")
 # tokenizer = LlamaTokenizer.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF")
 
-print("#### Model & Tokenizer loaded !!")
+# print("#### Model & Tokenizer loaded !!")
 
 pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, device=0, max_new_tokens = 1024)
 llm = HuggingFacePipeline(pipeline=pipe)
 
-print("#### Pipeline created !!")
+# print("#### Pipeline created !!")
 
 
 sum_chain = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type='stuff',
     retriever=db.as_retriever(search_kwargs={'k': 2}),
-    return_source_documents=False,
+    return_source_documents=True,
     chain_type_kwargs={'prompt': prompt}
 )
 
 
-
 def generate_response(query):
     response = sum_chain({'query': query})
-    print("#### Summary generated successfully !!")
+
+    # print("#### Summary generated successfully !!")
     generated_text = response['result'].strip()
     if 'Summary:' in generated_text:
         summary = generated_text.split('Summary:')[-1].strip()
     else:
         summary = generated_text
-    return summary
+
+    if 'Context : ':
+        text = generated_text.split('Context :')[-1].strip()
+        retrieved_docs = text.split('Question :')[0].strip()
+    else:
+        retrieved_docs = ""
+
+    return summary, retrieved_docs
