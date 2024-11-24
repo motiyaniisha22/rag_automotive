7c6bf7aef3f85e95a82b4a78ece23ee186756780
Isha Motiyani <130890395+motiyaniisha22@users.noreply.github.com>
2024-11-24 16:25:13 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,19 +2,11 @@ import time
 import api
 import argparse
 
-# Set up argument parser
 parser = argparse.ArgumentParser(description="Summarization")
 parser.add_argument('--json_query', type=str, help="JSON string with automobile issue and car details", required=True)
 
-# Parse the command line argument
 args = parser.parse_args()
 user_input = args.json_query
-# user_input = "{'make': 'ford', 'model' : 'escape', 'year': '2001', 'issue': 'stuck throttle risk'}"
-
-# while True:
-#     user_input = input("Paste Json here: ")
-#     if user_input == "exit":
-#         break
 
 # start_time = time.time()
 response, retrieved_docs = api.generate_response(user_input)
